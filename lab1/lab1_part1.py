8feeea394eb129d366c8165c98af979ae8d0e0af
sean walters <sean.m.walters4.civ@us.navy.mil>
2024-04-06 19:08:02 -0700

    ready to go

--- lab1/lab1_part1.py
+++ lab1/lab1_part1.py
@@ -1,23 +1,24 @@
-#An empty string  or a one character string is also a palindrome,
+#An empty string or a one character string are palindrome,
 #since it "reads" the same forward and backward.
 is_palindrome = True
+
+#user input
 input_str = input("Enter a string: ")
 
-#Extra Credit
+#Extra Credit: Case insensitive,  remove spaces to support sentence-long. 
 s = input_str.replace(" ", "").lower()
-
 left = 0
 right = len(s) - 1
-
-
-#print (left)
-#print(right)
+#loop
 while left < right:
     if s[left] != s[right]:
+        #exspected match if not set false and stop loop
         is_palindrome = False
         break
     left += 1
     right -= 1
-    
+    #string is palindrome search string both directons
+
+#output    
 print(f"Is '{input_str}' a palindrome? {is_palindrome}")
 
