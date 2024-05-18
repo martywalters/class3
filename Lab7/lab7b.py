8b5ecd82a9644d64bb783f4ba15dcdff6b0f53e6
marty <martywalters@yahoo.com>
2024-05-17 20:10:38 -0700

    done2

--- Lab7/lab7b.py
+++ Lab7/lab7b.py
@@ -1,3 +1,4 @@
+from time import sleep
 class Restaurant:
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, 'instance'):
@@ -48,9 +49,13 @@ class Cheeseburger(Food):
         return 5.99
     def prepare(self):
         print( f"{__class__.__name__}: grill all-beef patty")
+        sleep(1)
         print("Cheeseburger: flip patty")
+        sleep(2)
         print("Cheeseburger: put cheese on patty")
+        sleep(2)
         print("Cheeseburger: put patty on bun and add toppings")
+        sleep(1)
         print("Cheeseburger: All done!")
 
 class Pasta(Food):
@@ -58,19 +63,28 @@ class Pasta(Food):
         return 8.99
     def prepare(self):
         print("Pasta: boil water for noodles")
+        sleep(2)
         print("Pasta: saute onions, garlic and tomatoes for sauce")
+        sleep(2)
         print("Pasta: put noodles in water")
+        sleep(2)
         print("Pasta: season the sauce")
+        sleep(1)
         print("Pasta: plate noodles and add sauce on top")
+        sleep(1)
         print("Pasta: All done!")
 class HotDog(Food):
     def price(self):
         return 3.99
     def prepare(self):
         print("HotDog: boil water for dog")
+        sleep(2)
         print("HotDog: put dog in boiling water")
+        sleep(2)
         print("HotDog: plate an open bun")
+        sleep(1)
         print("HotDog: put dog in bun")
+        sleep(1)
         print("HotDog: put ketchup and mustard on dog in bun")
 def main():
     r = Restaurant()
@@ -87,12 +101,13 @@ def main():
     if food:
          print(food)
     print(r)
+    # Use this test to prove we have a single instance of Restaurant:
+    r2 = Restaurant()
+    print(r2)
 if __name__ == "__main__":
     main()
 
-# Use this test to prove we have a single instance of Restaurant:
-r2 = Restaurant()
-print(r2)
+
 
 
     
